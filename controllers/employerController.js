5755510a19cda5a32d921ad014b29df5b266201f
willy1669 <williamschinonso11@gmail.com>
2019-04-14 22:53:21 +0100

    required the needed modules

--- controllers/employerController.js
+++ controllers/employerController.js
@@ -1,7 +1,9 @@
-const model = require('../models/employer');
+const employer = require('../models/employer');
 const joi = require('joi');
 const passwordHash = require('password-hash');
 const service = require('../services/employerService');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
 //Define schema for validating user input
 const schema = joi.object().keys({
@@ -51,3 +53,58 @@ exports.getEmployers = (req, res,) => {
         console.log("Error : "+exception);
     }
 }
+
+function isValidPassword(user, password){
+    return passwordHash.verify(password, user.password);
+}
+
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+});
+
+passport.deserializeUser(function(id, done) {
+    employer.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+exports.loginUser = function (req, res) {
+    passport.authenticate('login', {
+    successRedirect: '/users', 
+    failureRedirect: '/login'
+    });
+    try {
+        passport.use ('login', new LocalStrategy(
+            employer.findOne({email: req.body.email}, function (err, user) {
+                if (err) {
+                    res.json({err: err});
+                }
+                if (user && isValidPassword(user, req.body.password)) {
+                    var token = jwt.sign({email: user.email, id: user._id}, '+secret+', {expiresIn: '12h'});
+                    res.json({userId:user._id, email:user.email, username: user.username, token: token, message: 'Login successful.'});
+                }
+                else {
+                    res.json({message: 'Incorrect email or password.'});
+                    console.log(isValidPassword (req.body.password));
+                }
+                console.log(user);
+            }),
+        ));
+    }
+    catch (exception) {
+        console.log(exception);
+    }
+}
+// exports.employerPostGig = (req, res) => {
+//     var data = {
+//         gig: [],
+//         employer: req.body.employer,
+//         time: Date.now()
+//     }
+//     try {
+//         return service.addGigs
+//     }   catch(exception) {
+//         console.log("Error : "+exception);
+//     }
+
+// }
