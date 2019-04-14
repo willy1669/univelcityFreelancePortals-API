ec44f9a80d8b186732930840621cec86c733f632
willy1669 <williamschinonso11@gmail.com>
2019-04-14 08:42:31 +0100

    create getAllUsers Route

--- routers/employerRouter.js
+++ routers/employerRouter.js
@@ -5,6 +5,7 @@ const employerController = require('../controllers/employerController');
 //GET graduate listing.
 router.post('/signUp', employerController.employerSignUp);
 router.get('/employer/:id', employerController.getEmployerById);
+router.get('/', employerController.getEmployers);
 
 
 module.exports = router;
