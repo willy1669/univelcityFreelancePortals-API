4844f11506d7a107837928ad81b936fafd23886e
willy1669 <williamschinonso11@gmail.com>
2019-04-15 07:30:22 +0100

    worked on addGig function and added getAllGigs function

--- services/gigsService.js
+++ services/gigsService.js
@@ -1,16 +1,30 @@
 const model = require('../models/gigs');
-const employerRepo = require('../repositories/employerRepository');
+const emp = require('../models/employer');
+const Repo = require('../repositories/employerRepository');
 const repository = require('../repositories/gigsRepository');
+const employerControler = require('../controllers/employerController');
+
 
 exports.addGigs = (req, res, data) => {
-    repository.add(data, (err, gig) => {
-        if (err) {
-            res.json({err: err})
-        }
-        employerRepo.getById(data.employer, (err, employers) => {
+    repository.add(data, function (err, gig) {
+        Repo.getById(data.employer, function (err, employer) {
+            employer.gigs.push(gig._id)
             if (err) res.json({err: err, message: 'error, gig could not be added'});
             res.json ({message: 'gig created successfully'});
-            console.log(data)
         })
     })
+} 
+
+exports.getAllGigs = (req, res, options) => {
+    repository.getAll(options, '-__v', (err, Employers) => {
+        if (err) res.json({err:err, message:'error, could not retrieve employers'});
+        res.json(Employers);
+    });
+}
+
+exports.getGigById = (req, res, id) => {
+    repository.getById(id, function (err, gig){
+        if (err) res.json ({err: err, message: 'error, could not get gig by id'});
+        res.json ({Gig: gig});
+    });
 }
