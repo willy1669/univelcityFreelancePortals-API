8512b7c7357327a0aa2c938dd0f024b0d518c5a2
willy1669 <williamschinonso11@gmail.com>
2019-05-07 15:04:57 +0100

    modified addGigs function

--- controllers/gigsController.js
+++ controllers/gigsController.js
@@ -4,13 +4,14 @@ const category = require('../models/category');
 const service = require('../services/gigsService');
 
 exports.addGigs = (req, res) => {
-const data = {
-    employer: req.body.employer,
-    jobDescription: req.body.jobDescription,
-    time: Date.now()
-}
+    const employer = req.body.employer;
+    const data = {
+        jobDescription: req.body.jobDescription,
+        time: Date.now(),
+        title: req.body.title
+    }
     try {
-        return service.addGigs(req, res, data)
+        return service.addGigs(req, res, data, employer)
     }
     catch(exception) {
         console.log("Error: " +exception);
@@ -29,3 +30,12 @@ exports.getGigById = function (req, res){
     var id = req.params.id;
     return service.getGigById(req, res, id);
 }
+
+exports.searchGigs = function (req, res){
+    try {
+        var options = req.query.title;
+        return service.searchByTitle(req, res, options);
+    } catch (exception){
+        console.log("Error : "+exception);
+    }
+} 
